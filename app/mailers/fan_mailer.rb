7eb781ec8b052b631514bc3e0595dd50f75b2fa8
alexheil <aheil4250@gmail.com>
2017-09-29 14:38:46 -0400

    fan email blasts

--- app/mailers/fan_mailer.rb
+++ app/mailers/fan_mailer.rb
@@ -2,6 +2,50 @@ class FanMailer < ApplicationMailer
 
 	def post_email(fan)
 	  @fan = fan
+	  @aritst = artist
+	  @post = artist_post
+	  mail(to: @fan.email, subject: 'Sent some posts | Noisaea')
+	end
+
+	def photo_email(fan)
+	  @fan = fan
+	  @aritst = artist
+	  @photo = artist_photo
+	  mail(to: @fan.email, subject: 'Sent some posts | Noisaea')
+	end
+
+	def video_email(fan)
+	  @fan = fan
+	  @aritst = artist
+	  @video = artist_video
+	  mail(to: @fan.email, subject: 'Sent some posts | Noisaea')
+	end
+
+	def merch_email(fan)
+	  @fan = fan
+	  @aritst = artist
+	  @merch = artist_merch
+	  mail(to: @fan.email, subject: 'Sent some posts | Noisaea')
+	end
+
+	def tour_email(fan)
+	  @fan = fan
+	  @aritst = artist
+	  @tour = artist_tour
+	  mail(to: @fan.email, subject: 'Sent some posts | Noisaea')
+	end
+
+	def merch_email(fan)
+	  @fan = fan
+	  @aritst = artist
+	  @item = artist_item
+	  mail(to: @fan.email, subject: 'Sent some posts | Noisaea')
+	end
+
+	def album_email(fan)
+	  @fan = fan
+	  @aritst = artist
+	  @album = artist_album
 	  mail(to: @fan.email, subject: 'Sent some posts | Noisaea')
 	end
 
