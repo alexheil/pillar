d75f64fd1844a1a5b89c5b480f344d723cf0ad7a
alexheil <aheil4250@gmail.com>
2017-10-05 00:07:36 -0400

    tour and show

--- db/migrate/20171003195425_create_artist_tours.rb
+++ db/migrate/20171003195425_create_artist_tours.rb
@@ -3,7 +3,10 @@ class CreateArtistTours < ActiveRecord::Migration[5.0]
     create_table :artist_tours do |t|
     	t.references :artist
     	t.string :title
-    	
+    	t.text :description
+    	t.integer :amount_of_shows
+    	t.string :artists
+    	t.string :image
     	t.string :slug
 
       t.timestamps
--- db/migrate/20171003195431_create_artist_shows.rb
+++ db/migrate/20171003195431_create_artist_shows.rb
@@ -1,6 +1,18 @@
 class CreateArtistShows < ActiveRecord::Migration[5.0]
   def change
     create_table :artist_shows do |t|
+    	t.references :artist
+    	t.references :artist_tours
+    	t.string :title
+    	t.integer :month
+      t.integer :day
+      t.integer :year
+      t.string :venue
+      t.string :location
+      t.text :description
+      t.string :ticket_url
+      t.string :image
+      t.string :slug
 
       t.timestamps
     end
